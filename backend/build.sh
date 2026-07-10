95d200a5f9d46e6f1d691c7cf6ed5d0a50d1bfdd
Chigozie Chinonso <chigoziejohnc76@gmail.com>
2026-07-10 13:56:37 +0100

    Promote trentjoshuaeee@gmail.com to superuser on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -8,3 +8,14 @@ python manage.py migrate
 python manage.py seed_clusters
 python manage.py seed_products
 python manage.py seed_combos
+python manage.py shell -c "
+from apps.users.models import User
+u = User.objects.filter(email='trentjoshuaeee@gmail.com').first()
+if u:
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('Admin promoted:', u.email)
+else:
+    print('User not found')
+"
